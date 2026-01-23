1dbe1f3abf2ead420656b31828ad9e32ba5b9e8c
Gemma Danks <8269577+gemmadanks@users.noreply.github.com>
2026-01-23 19:34:52 +0000

    chore: add docstring

--- src/starbox/visibility.py
+++ src/starbox/visibility.py
@@ -6,6 +6,18 @@ import numpy as np
 
 @dataclass
 class VisibilitySet:
+    """A data class representing a set of visibilities.
+
+    Attributes:
+        vis: Complex visibilities with shape (time, baseline, chan).
+        uvw_m: UVW coordinates in meters with shape (time, baseline, 3).
+        station1: Indices of the first station for each baseline.
+        station2: Indices of the second station for each baseline.
+        times_mjd: Times of the observations in Modified Julian Date.
+        freqs_hz: Frequencies of the channels in Hz.
+        weights: Weights for each visibility with shape (time, baseline, chan).
+    """
+
     vis: np.ndarray  # (time, baseline, chan)
     uvw_m: np.ndarray  # (time, baseline, 3)
     station1: np.ndarray  # (station,)
